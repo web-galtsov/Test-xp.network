9fe00069b8a3efa2c4ed3d63c1c16c054f81d085
web-galtsov <oksana.galtsov@gmail.com>
2022-03-22 11:59:13 +0200

    edit new  data.js

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 const fakeData = [
     {
         id: 1,
-        total_hours: "159:00",
-        hours: "155:00",
-        manual_hours: "4:00",
-        extraordinary_hours: "4:00",
+        total_hours: "16759:00",
+        hours: "7895:00",
+        manual_hours: "344:00",
+        extraordinary_hours: "78:00",
         worker_name: "יואב שרון",
         id_number: "203483924"
     },
@@ -21,8 +21,8 @@ const fakeData = [
         id: 3,
         total_hours: "159:00",
         hours: "155:00",
-        manual_hours: "4:00",
-        extraordinary_hours: "4:00",
+        manual_hours: "34:00",
+        extraordinary_hours: "74:00",
         worker_name: "יואב שרון",
         id_number: "203483924"
     },
@@ -30,26 +30,26 @@ const fakeData = [
         id: 4,
         total_hours: "159:00",
         hours: "155:00",
-        manual_hours: "4:00",
-        extraordinary_hours: "4:00",
+        manual_hours: "84:00",
+        extraordinary_hours: "54:00",
         worker_name: "יואב שרון",
         id_number: "203483924"
     },
     {
         id: 5,
         total_hours: "159:00",
-        hours: "155:00",
-        manual_hours: "4:00",
-        extraordinary_hours: "4:00",
+        hours: "9855:00",
+        manual_hours: "34:00",
+        extraordinary_hours: "54:00",
         worker_name: "יואב שרון",
         id_number: "203483924"
     },
     {
         id: 6,
         total_hours: "159:00",
-        hours: "155:00",
-        manual_hours: "4:00",
-        extraordinary_hours: "4:00",
+        hours: "955:00",
+        manual_hours: "46:00",
+        extraordinary_hours: "9:00",
         worker_name: "יואב שרון",
         id_number: "203483924"
     },
@@ -65,7 +65,7 @@ const fakeData = [
     {
         id: 8,
         total_hours: "159:00",
-        hours: "155:00",
+        hours: "122:00",
         manual_hours: "4:00",
         extraordinary_hours: "4:00",
         worker_name: "יואב שרון",
@@ -74,7 +74,7 @@ const fakeData = [
     {
         id: 9,
         total_hours: "153:00",
-        hours: "155:00",
+        hours: "195:00",
         manual_hours: "4:00",
         extraordinary_hours: "4:00",
         worker_name: "יואב שרון",
@@ -83,17 +83,17 @@ const fakeData = [
     {
         id: 10,
         total_hours: "159:00",
-        hours: "155:00",
+        hours: "855:00",
         manual_hours: "4:00",
-        extraordinary_hours: "4:00",
+        extraordinary_hours: "2:00",
         worker_name: "יואב שרון",
         id_number: "203483924"
     },
     {
         id: 11,
-        total_hours: "159:00",
-        hours: "155:00",
-        manual_hours: "4:00",
+        total_hours: "152:00",
+        hours: "225:00",
+        manual_hours: "445:00",
         extraordinary_hours: "",
         worker_name: "יואב שרון",
         id_number: "203483924"
